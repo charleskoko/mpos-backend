05b776595f1d3952d017e7bf32daca0b4ba01372
Charles Koko <55494612+charleskoko@users.noreply.github.com>
2022-10-07 22:55:05 +0000

    add frontend page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/privacy-policy', function (){return view('privacyPolicy');})->name('privacy-policy');
