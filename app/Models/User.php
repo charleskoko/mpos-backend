5c237964bb649d1bede11bacb5f77631c8b6121c
Charles Koko <55494612+charleskoko@users.noreply.github.com>
2022-11-05 11:10:32 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,7 +54,7 @@ class User extends Authenticatable
 
     public function orders(): HasMany
     {
-        return $this->hasMany(Order::class);
+        return $this->hasMany(Order::class)->orderBy('created_at');
     }
 
     public function invoices(): HasMany
